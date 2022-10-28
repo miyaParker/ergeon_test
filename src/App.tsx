2da3fee41f42888972d52f4b0ccab5d11c768b01
miyaParker <33570710+miyaParker@users.noreply.github.com>
2022-10-28 18:14:04 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import React from "react";
+export default function App(): JSX.Element {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
